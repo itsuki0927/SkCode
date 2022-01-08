9c08d974730b19a6cc841baead323763e42e4ba2
itsuki0927 <2309899048@qq.com>
2022-01-08 15:29:06 +0800

    feat: add css、html、json、lua、ts language servers

--- lua/itsuki/lsp/install.lua
+++ lua/itsuki/lsp/install.lua
@@ -1,6 +1,12 @@
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) 
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local lspconfig = require('lspconfig')
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
 
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -33,15 +39,47 @@ local on_attach = function(client, bufnr)
 end
 
 -- map buffer local keybindings when the language server attaches
-local servers = { 'html', 'sumneko_lua', 'tsserver', 'cssls' }
+local servers = { 'html', 'tsserver', 'cssls', 'eslint', 'jsonls', 'emmet_ls', 'sumneko_lua' }
 
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    flags = {
-      debounce_text_changes = 150,
+for _, server in ipairs(servers) do
+  if server == 'sumneko_lua' then
+    lspconfig.sumneko_lua.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      flags = {
+        debounce_text_changes = 150,
+      },
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = runtime_path,
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'},
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+            enable = false,
+          },
+        }
+      }
     }
-  }
+  else
+    lspconfig[server].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      flags = {
+        debounce_text_changes = 150,
+      }
+    }
+  end
 end
 
