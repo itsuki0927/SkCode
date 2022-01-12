3793fbaa3f1f9986aee456c6dcb9e0315fd2f783
itsuki0927 <2309899048@qq.com>
2022-01-12 17:00:13 +0800

    chore: reduce bufferline options

--- lua/plugins/configs/bufferline.lua
+++ lua/plugins/configs/bufferline.lua
@@ -8,32 +8,25 @@ end
 
 configs.setup({
   options = {
-    -- numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
     numbers = function(opts)
       return string.format('%s', opts.raise(opts.ordinal))
     end,
-    close_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
-    right_mouse_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
-    left_mouse_command = 'buffer %d', -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-    indicator_icon = '▎',
     buffer_close_icon = '',
-    modified_icon = '●',
+    modified_icon = '',
     close_icon = '',
     left_trunc_marker = '',
     right_trunc_marker = '',
-    max_name_length = 30,
-    max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-    tab_size = 21,
-    diagnostics = false, -- | "nvim_lsp" | "coc",
-    diagnostics_update_in_insert = false,
+    max_name_length = 14,
+    max_prefix_length = 13,
+    tab_size = 20,
+    diagnostics = false,
     offsets = { { filetype = 'NvimTree', text = '', padding = 1 } },
     show_buffer_icons = true,
     show_buffer_close_icons = true,
     show_close_icon = true,
     show_tab_indicators = true,
-    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    separator_style = 'thin', -- | "thick" | "thin" | { 'any', 'any' },
+    persist_buffer_sort = true,
+    separator_style = 'thin',
     enforce_regular_tabs = true,
     always_show_bufferline = true,
   },
@@ -129,18 +122,3 @@ configs.setup({
     },
   },
 })
-
-vim.cmd([[
-nnoremap <silent><TAB> :BufferLineCycleNext<CR>
-nnoremap <silent><S-TAB> :BufferLineCyclePrev<CR>
-
-nnoremap <silent><leader>1 <Cmd>BufferLineGoToBuffer 1<CR>
-nnoremap <silent><leader>2 <Cmd>BufferLineGoToBuffer 2<CR>
-nnoremap <silent><leader>3 <Cmd>BufferLineGoToBuffer 3<CR>
-nnoremap <silent><leader>4 <Cmd>BufferLineGoToBuffer 4<CR>
-nnoremap <silent><leader>5 <Cmd>BufferLineGoToBuffer 5<CR>
-nnoremap <silent><leader>6 <Cmd>BufferLineGoToBuffer 6<CR>
-nnoremap <silent><leader>7 <Cmd>BufferLineGoToBuffer 7<CR>
-nnoremap <silent><leader>8 <Cmd>BufferLineGoToBuffer 8<CR>
-nnoremap <silent><leader>9 <Cmd>BufferLineGoToBuffer 9<CR>
-]])
