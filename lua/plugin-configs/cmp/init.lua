f08f6d26b72abb89541640d8485b510c1921ddc3
itsuki0927 <2309899048@qq.com>
2022-05-24 08:08:57 +0800

    feat: add cmp border

--- lua/plugin-configs/cmp/init.lua
+++ lua/plugin-configs/cmp/init.lua
@@ -3,6 +3,18 @@ vim.g.completeopt = 'menu,menuone,noselect,noinsert'
 local present, cmp = pcall(require, 'cmp')
 
 if present then
+  local function border(hl_name)
+    return {
+      { '┌', hl_name },
+      { '─', hl_name },
+      { '┐', hl_name },
+      { '│', hl_name },
+      { '┘', hl_name },
+      { '─', hl_name },
+      { '└', hl_name },
+      { '│', hl_name },
+    }
+  end
   local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
@@ -13,6 +25,14 @@ if present then
       expand = function(args)
         require('luasnip').lsp_expand(args.body)
       end,
+    },
+    window = {
+      completion = {
+        border = border('CmpBorder'),
+      },
+      documentation = {
+        border = border('CmpDocBorder'),
+      },
     },
     experimental = {
       native_menu = false,
