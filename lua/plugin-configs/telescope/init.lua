b9b50b7f3fa92e2fae39a99e9f03ab36613e207e
itsuki0927 <2309899048@qq.com>
2022-06-13 16:32:21 +0800

    feat: add telescope multi selection

--- lua/plugin-configs/telescope/init.lua
+++ lua/plugin-configs/telescope/init.lua
@@ -2,6 +2,35 @@ local present, telescope = pcall(require, 'telescope')
 
 local actions = require('telescope.actions')
 local action_layout = require('telescope.actions.layout')
+local action_state = require('telescope.actions.state')
+
+local telescope_custom_actions = {}
+
+function telescope_custom_actions._multiopen(prompt_bufnr, open_cmd)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local num_selections = #picker:get_multi_selection()
+  if not num_selections or num_selections <= 1 then
+    actions.add_selection(prompt_bufnr)
+  end
+  actions.send_selected_to_qflist(prompt_bufnr)
+  vim.cmd('cfdo ' .. open_cmd)
+end
+
+function telescope_custom_actions.multi_selection_open_vsplit(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, 'vsplit')
+end
+
+function telescope_custom_actions.multi_selection_open_split(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, 'split')
+end
+
+function telescope_custom_actions.multi_selection_open_tab(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, 'tabe')
+end
+
+function telescope_custom_actions.multi_selection_open(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, 'edit')
+end
 
 if present then
   telescope.setup({
@@ -63,10 +92,10 @@ if present then
           ['<esc>'] = actions.close,
           ['<C-[>'] = actions.close,
 
-          ['<CR>'] = actions.select_default,
-          ['<C-x>'] = actions.select_horizontal,
-          ['<C-v>'] = actions.select_vertical,
-          ['<C-t>'] = actions.select_tab,
+          ['<CR>'] = telescope_custom_actions.multi_selection_open,
+          ['<C-x>'] = telescope_custom_actions.multi_selection_open_split,
+          ['<C-v>'] = telescope_custom_actions.multi_selection_open_vsplit,
+          ['<C-t>'] = telescope_custom_actions.multi_selection_open_tab,
 
           ['<C-u>'] = actions.preview_scrolling_up,
           ['<C-d>'] = actions.preview_scrolling_down,
@@ -85,10 +114,10 @@ if present then
           ['<esc>'] = actions.close,
           ['<C-[>'] = actions.close,
 
-          ['<CR>'] = actions.select_default,
-          ['<C-x>'] = actions.select_horizontal,
-          ['<C-v>'] = actions.select_vertical,
-          ['<C-t>'] = actions.select_tab,
+          ['<CR>'] = telescope_custom_actions.multi_selection_open,
+          ['<C-x>'] = telescope_custom_actions.multi_selection_open_split,
+          ['<C-v>'] = telescope_custom_actions.multi_selection_open_vsplit,
+          ['<C-t>'] = telescope_custom_actions.multi_selection_open_tab,
 
           ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
           ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
