2620ba94b082c612876edf95e4bab1ba6760e1b2
itsuki0927 <2309899048@qq.com>
2022-06-20 13:23:27 +0800

    chore: clean skcode util

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -60,38 +60,6 @@ skcode.map = function(mode, keys, command, opt)
   map_wrapper(mode, keys, command, options)
 end
 
--- 懒加载包
-skcode.packer_lazy_load = function(plugin, timer)
-  if plugin then
-    timer = timer or 0
-    vim.defer_fn(function()
-      require('packer').loader(plugin)
-    end, timer)
-  end
-end
-
--- 隐藏statusline
-skcode.hide_statusline = function()
-  local hidden = skcode.load_config().plugins.options.statusline.hidden
-  local shown = skcode.load_config().plugins.options.statusline.shown
-  local api = vim.api
-  local buftype = api.nvim_buf_get_option(0, 'ft')
-
-  -- tbl_contains: 检查列表(t)是否包含`v`
-  if vim.tbl_contains(shown, buftype) then
-    -- 设置全局值laststatus
-    api.nvim_set_option('laststatus', 2)
-    return
-  end
-
-  if vim.tbl_contains(hidden, buftype) then
-    api.nvim_set_option('laststatus', 0)
-    return
-  end
-
-  api.nvim_set_option('laststatus', 2)
-end
-
 skcode.merge = function(...)
   return vim.tbl_deep_extend('force', ...)
 end
