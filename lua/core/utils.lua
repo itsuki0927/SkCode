3e27b4b4a94a3a96431932b3e30366262187d57c
itsuki0927 <2309899048@qq.com>
2022-07-15 10:02:15 +0800

    chore: clean map util

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -7,55 +7,23 @@ end
 
 -- 键位定义函数
 -- @param mode 模式
--- @param keys 键位
+-- @param key 键位
 -- @param command 命令
 -- @param opt 其他参数
-skcode.map = function(mode, keys, command, opt)
+skcode.map = function(mode, key, command, opts)
   local options = { noremap = true, silent = true }
-  if opt then
-    options = vim.tbl_extend('force', options, opt)
+  if opts then
+    options = skcode.merge(options, opts)
   end
+  vim.api.nvim_set_keymap(mode, key, command, options)
+end
 
-  -- all valid modes allowed for mappings
-  -- :h map-modes
-  local valid_modes = {
-    [''] = true,
-    ['n'] = true,
-    ['v'] = true,
-    ['s'] = true,
-    ['x'] = true,
-    ['o'] = true,
-    ['!'] = true,
-    ['i'] = true,
-    ['l'] = true,
-    ['c'] = true,
-    ['t'] = true,
-  }
-
-  -- helper function for M.map
-  -- can gives multiple modes and keys
-  local function map_wrapper(sub_mode, lhs, rhs, sub_options)
-    if type(lhs) == 'table' then
-      for _, key in ipairs(lhs) do
-        map_wrapper(sub_mode, key, rhs, sub_options)
-      end
-    else
-      if type(sub_mode) == 'table' then
-        for _, m in ipairs(sub_mode) do
-          map_wrapper(m, lhs, rhs, sub_options)
-        end
-      else
-        if valid_modes[sub_mode] and lhs and rhs then
-          vim.api.nvim_set_keymap(sub_mode, lhs, rhs, sub_options)
-        else
-          sub_mode, lhs, rhs = sub_mode or '', lhs or '', rhs or ''
-          print("Cannot set mapping [ mode = '" .. sub_mode .. "' | key = '" .. lhs .. "' | cmd = '" .. rhs .. "' ]")
-        end
-      end
-    end
+skcode.buf_map = function(bufnr, mode, key, command, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = skcode.merge(options, opts)
   end
-
-  map_wrapper(mode, keys, command, options)
+  vim.api.nvim_buf_set_keymap(bufnr, mode, key, command, options)
 end
 
 skcode.merge = function(...)
