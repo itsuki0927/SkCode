d3b920c10fc5e11bd8526c8efa34706f16216e41
itsuki0927 <2309899048@qq.com>
2022-09-25 12:55:02 +0800

    fix: fix colorizer options error

--- lua/plugin-configs/colorizer/init.lua
+++ lua/plugin-configs/colorizer/init.lua
@@ -1,19 +1,25 @@
 local present, colorizer = pcall(require, 'colorizer')
 
 if present then
-  colorizer.setup({ '*' }, {
-    RGB = true, -- #RGB hex codes
-    RRGGBB = true, -- #RRGGBB hex codes
-    names = true, -- "Name" codes like Blue
-    RRGGBBAA = true, -- #RRGGBBAA hex codes
-    rgb_fn = true, -- CSS rgb() and rgba() functions
-    hsl_fn = true, -- CSS hsl() and hsla() functions
-    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-
-    -- Available modes: foreground, background
-    mode = 'background', -- Set the display mode.
+  colorizer.setup({
+    filetype = { '*' },
+    user_default_options = {
+      RGB = true, -- #RGB hex codes
+      RRGGBB = true, -- #RRGGBB hex codes
+      names = true, -- "Name" codes like Blue or blue
+      RRGGBBAA = true, -- #RRGGBBAA hex codes
+      AARRGGBB = true, -- 0xAARRGGBB hex codes
+      rgb_fn = true, -- CSS rgb() and rgba() functions
+      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      -- Available modes for `mode`: foreground, background,  virtualtext
+      mode = 'background', -- Set the display mode.
+      -- Available methods are false / true / "normal" / "lsp" / "both"
+      -- True is same as normal
+      tailwind = true, -- Enable tailwind colors
+      -- parsers can contain values used in |user_default_options|
+      --[[ sass = { enable = false, parsers = { css } }, -- Enable sass colors ]]
+    },
   })
-
-  vim.cmd('ColorizerReloadAllBuffers')
 end
