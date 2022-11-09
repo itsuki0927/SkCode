c728d23f8e4c27c5a1106efe97a9812521f49698
itsuki0927 <2309899048@qq.com>
2022-11-09 13:56:20 +0800

    chore: update treesitter inc selection init keymap

--- lua/plugin-configs/treesitter/init.lua
+++ lua/plugin-configs/treesitter/init.lua
@@ -34,7 +34,7 @@ if present then
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = '<C-n>',
+        init_selection = 'gnn',
         node_incremental = '<C-n>',
         scope_incremental = '<C-s>',
         node_decremental = '<C-r>',
