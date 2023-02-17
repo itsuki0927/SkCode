cf7771e526e9884e9844dd9b6b486a4b332e1a26
itsuki0927 <2309899048@qq.com>
2023-02-17 19:21:15 +0800

    fix: cmp cmdline not work & remove cmp completeoptions

--- lua/core/plugin/list.lua
+++ lua/core/plugin/list.lua
@@ -129,6 +129,7 @@ require('lazy').setup({
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-nvim-lua',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
     },
   },
 
--- lua/plugin-configs/cmp/init.lua
+++ lua/plugin-configs/cmp/init.lua
@@ -1,5 +1,3 @@
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-
 local present, cmp = pcall(require, 'cmp')
 
 if present then
@@ -29,7 +27,6 @@ if present then
       ['<C-j>'] = cmp.mapping.select_next_item(),
       ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
-      --[[ ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }), ]]
       ['<C-e>'] = cmp.mapping({
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
@@ -106,11 +103,7 @@ if present then
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' },
-    }, {
-      { name = 'cmdline' },
-    }),
+    sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } }),
   })
 end
 
