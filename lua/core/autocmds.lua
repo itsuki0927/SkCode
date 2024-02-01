4c0846b7348ee54330799eb9087e260b575c3822
liusenlin <liusenlin.0927@bytedance.com>
2024-02-01 23:02:44 +0800

    chore: add open file at the last position it was edited earlier

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -28,3 +28,13 @@ autocmd('BufEnter', {
   pattern = '*',
   command = 'set fo-=c fo-=r fo-=o',
 })
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Open file at the last position it was edited earlier',
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
