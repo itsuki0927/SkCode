b9538c60c5f95959dec9af2ff1f72fd95da677e9
itsuki0927 <2309899048@qq.com>
2025-03-09 20:58:20 +0800

    chore: add blank line hi

--- lua/config/plugins/blankline.lua
+++ lua/config/plugins/blankline.lua
@@ -1,5 +1,3 @@
-dofile(vim.g.base46_cache .. 'blankline')
-
 return {
   {
     'lukas-reineke/indent-blankline.nvim',
@@ -7,27 +5,57 @@ return {
     event = 'BufReadPost',
     ---@module "ibl"
     ---@type ibl.config
-    opts = {
-      indent = { char = '│', highlight = 'IblChar' },
-      scope = { char = '│', highlight = 'IblScopeChar' },
-      exclude = {
-        buftypes = { 'nofile', 'prompt', 'quickfix', 'terminal' },
-        filetypes = {
-          'aerial',
-          'alpha',
-          'dashboard',
-          'help',
-          'lazy',
-          'mason',
-          'neo-tree',
-          'NvimTree',
-          'neogitstatus',
-          'notify',
-          'startify',
-          'toggleterm',
-          'Trouble',
+
+    config = function()
+      dofile(vim.g.base46_cache .. 'blankline')
+      local highlight = {
+        'RainbowRed',
+        'RainbowYellow',
+        'RainbowBlue',
+        'RainbowOrange',
+        'RainbowGreen',
+        'RainbowViolet',
+        'RainbowCyan',
+      }
+
+      local base46 = require('base46')
+      local theme_colors = base46.get_theme_tb('base_30')
+
+      local hooks = require('ibl.hooks')
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = theme_colors.red })
+        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = theme_colors.yellow })
+        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = theme_colors.blue })
+        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = theme_colors.orange })
+        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = theme_colors.green })
+        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = theme_colors.pink })
+        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = theme_colors.cyan })
+      end)
+
+      require('ibl').setup({
+        indent = { char = '│', highlight = 'IblChar' },
+        scope = { char = '│', highlight = highlight, show_start = true },
+        exclude = {
+          buftypes = { 'nofile', 'prompt', 'quickfix', 'terminal' },
+          filetypes = {
+            'aerial',
+            'alpha',
+            'dashboard',
+            'help',
+            'lazy',
+            'mason',
+            'neo-tree',
+            'NvimTree',
+            'neogitstatus',
+            'notify',
+            'startify',
+            'toggleterm',
+            'Trouble',
+          },
         },
-      },
-    },
+      })
+
+      dofile(vim.g.base46_cache .. 'blankline')
+    end,
   },
 }
