5c60ca8a5cf3de6db387bef88c4ea1b2508add67
itsuki0927 <2309899048@qq.com>
2025-03-12 14:06:04 +0800

    chore: add autocmd

--- init.lua
+++ init.lua
@@ -19,6 +19,34 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+local autocmd = vim.api.nvim_create_autocmd
+
+-- user event that loads after UIEnter + only if file buf is there
+autocmd({ 'UIEnter', 'BufReadPost', 'BufNewFile' }, {
+  group = vim.api.nvim_create_augroup('NvFilePost', { clear = true }),
+  callback = function(args)
+    local file = vim.api.nvim_buf_get_name(args.buf)
+    local buftype = vim.api.nvim_get_option_value('buftype', { buf = args.buf })
+
+    if not vim.g.ui_entered and args.event == 'UIEnter' then
+      vim.g.ui_entered = true
+    end
+
+    if file ~= '' and buftype ~= 'nofile' and vim.g.ui_entered then
+      vim.api.nvim_exec_autocmds('User', { pattern = 'FilePost', modeline = false })
+      vim.api.nvim_del_augroup_by_name('NvFilePost')
+
+      vim.schedule(function()
+        vim.api.nvim_exec_autocmds('FileType', {})
+
+        if vim.g.editorconfig then
+          require('editorconfig').config(args.buf)
+        end
+      end)
+    end
+  end,
+})
+
 require('config.lazy')
 
 dofile(vim.g.base46_cache .. 'defaults')
