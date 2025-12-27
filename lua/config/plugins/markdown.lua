c5f793d35b9d676d7f7e14e6920a108bc9b71bb9
itsuki0927 <2309899048@qq.com>
2025-12-27 09:49:55 +0800

    chore: add markdown

---
+++ lua/config/plugins/markdown.lua
@@ -0,0 +1,12 @@
+-- install with yarn or npm
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+}
