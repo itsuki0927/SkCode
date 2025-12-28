960ed16da35113aec9de6730735bb8419d4cd2ff
itsuki0927 <2309899048@qq.com>
2025-12-28 14:10:31 +0800

    chore: add goto-preview

--- lua/config/plugins/ui.lua
+++ lua/config/plugins/ui.lua
@@ -24,4 +24,69 @@ return {
       virtcolumn = '100',
     },
   },
+
+  {
+    'rmagatti/goto-preview',
+    dependencies = { 'rmagatti/logger.nvim' },
+    event = 'BufEnter',
+    config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+    keys = {
+      {
+        '<leader>pd',
+        "<cmd>lua require('goto-preview').goto_preview_definition()<CR>",
+        noremap = true,
+        desc = 'goto preview definition',
+      },
+      {
+        '<leader>pD',
+        "<cmd>lua require('goto-preview').goto_preview_declaration()<CR>",
+        noremap = true,
+        desc = 'goto preview declaration',
+      },
+      {
+        '<leader>pi',
+        "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>",
+        noremap = true,
+        desc = 'goto preview implementation',
+      },
+      {
+        '<leader>pt',
+        "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>",
+        noremap = true,
+        desc = 'goto preview type definition',
+      },
+      {
+        '<leader>pr',
+        "<cmd>lua require('goto-preview').goto_preview_references()<CR>",
+        noremap = true,
+        desc = 'goto preview references',
+      },
+      {
+        '<leader>P',
+        "<cmd>lua require('goto-preview').close_all_win()<CR>",
+        noremap = true,
+        desc = 'close all preview windows',
+      },
+    },
+    opts = {
+      references = {
+        provider = 'telescope',
+        telescope = require('telescope.themes').get_dropdown({
+          previewer = true,
+          initial_mode = 'insert',
+          sorting_strategy = 'ascending',
+          layout_strategy = 'horizontal',
+          layout_config = {
+            horizontal = {
+              prompt_position = 'top',
+              width = 0.95,
+              height = 0.8,
+              preview_width = 0.6,
+            },
+          },
+          borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+        }),
+      },
+    },
+  },
 }
